127e1d2c70731d575c9fc06fc65e40134a548822
Saradindu Sengupta <saradindu.mi1@iiitmk.ac.in>
2024-05-18 04:38:50 +0530

    feat: Add Docker script for running OpenFace and generating video features

---
+++ bin/openface.sh
@@ -0,0 +1,1 @@
+sudo docker run --rm -v "$PWD:/mnt" -it algebr/openface -c "cp /mnt/in.mp4 /tmp/video.mp4 && build/bin/FeatureExtraction -f /tmp/video.mp4 -2Dfp -out_dir /tmp && cp /tmp/video.csv /mnt/input_video.csv"
---
+++ bin/run.sh
@@ -0,0 +1,15 @@
+# Local
+python3 inference.py --mouth_region_size=256 --source_video_path=/home/saradindu/dev/facegen/src/DINet_optimized/asserts/examples/25fps/test1.mp4 --source_openface_landmark_path=/home/saradindu/dev/facegen/src/DINet_optimized/asserts/examples/test1.csv --driving_audio_path=/home/saradindu/dev/facegen/src/DINet_optimized/asserts/examples/driving_audio_1.wav --pretrained_clip_DINet_path=/home/saradindu/dev/facegen/src/DINet_optimized/asserts/clip_training_DINet_256mouth.pth --Wav2vecDS_model_path="/home/saradindu/dev/facegen/src/DINet_optimized/asserts/wav2vecDS.pt"
+
+# Paperspace run with output dir mentioned
+
+python3 inference.py --mouth_region_size=256 --source_video_path=/datasets/dinet_optimized_asserts/asserts/examples/25fps/test1.mp4 --source_openface_landmark_path=/datasets/dinet_optimized_asserts/asserts/examples/test1.csv --driving_audio_path=/datasets/dinet_optimized_asserts/asserts/examples/driving_audio_1.wav --pretrained_clip_DINet_path=/datasets/dinet_optimized_asserts/asserts/clip_training_DINet_256mouth.pth --Wav2vecDS_model_path="/datasets/dinet_optimized_asserts/asserts/wav2vecDS.pt" --res_video_dir="/datasets/dinet_optimized_asserts/inference_result"
+
+# Paperspace run without output directory mentioned
+python3 inference.py --mouth_region_size=256 --source_video_path=/datasets/dinet_optimized_asserts/asserts/examples/25fps/test5.mp4 --source_openface_landmark_path=/datasets/dinet_optimized_asserts/asserts/examples/test5.csv --driving_audio_path=/datasets/dinet_optimized_asserts/asserts/examples/driving_audio_5.wav --pretrained_clip_DINet_path=/datasets/dinet_optimized_asserts/asserts/clip_training_DINet_256mouth.pth --Wav2vecDS_model_path="/datasets/dinet_optimized_asserts/asserts/wav2vecDS.pt"
+
+
+python3 inference.py --mouth_region_size=256 --source_video_path=/datasets/dinet_optimized_asserts/asserts/examples/25fps/test1.mp4 --source_openface_landmark_path=/datasets/dinet_optimized_asserts/asserts/examples/test1.csv --driving_audio_path=/datasets/dinet_optimized_asserts/asserts/examples/Recording_1.wav --pretrained_clip_DINet_path=/datasets/dinet_optimized_asserts/asserts/clip_training_DINet_256mouth.pth --Wav2vecDS_model_path="/datasets/dinet_optimized_asserts/asserts/wav2vecDS.pt"
+
+
+python3 inference.py --mouth_region_size=256 --source_video_path=/datasets/dinet_optimized_asserts/test_videos/in.mp4 --source_openface_landmark_path=/datasets/dinet_optimized_asserts/test_videos/input_video.csv --driving_audio_path=/datasets/dinet_optimized_asserts/asserts/examples/driving_audio_2.wav --pretrained_clip_DINet_path=/datasets/dinet_optimized_asserts/asserts/clip_training_DINet_256mouth.pth --Wav2vecDS_model_path="/datasets/dinet_optimized_asserts/asserts/wav2vecDS.pt"
