fd27497c10498239aeb72bdb8925e68688e42c8a
Saradindu Sengupta <saradindu.mi1@iiitmk.ac.in>
2024-05-19 01:22:03 +0530

    Update video path in openface.sh

--- bin/openface.sh
+++ bin/openface.sh
@@ -1,1 +1,1 @@
-sudo docker run --rm -v "$PWD:/mnt" -it algebr/openface -c "cp /mnt/in.mp4 /tmp/video.mp4 && build/bin/FeatureExtraction -f /tmp/video.mp4 -2Dfp -out_dir /tmp && cp /tmp/video.csv /mnt/input_video.csv"
+sudo docker run --rm -v "$PWD:/mnt" -it algebr/openface -c "cp /mnt/w3-25fps.mp4 /tmp/video.mp4 && build/bin/FeatureExtraction -f /tmp/video.mp4 -2Dfp -out_dir /tmp && cp /tmp/video.csv /mnt/input_video.csv"
